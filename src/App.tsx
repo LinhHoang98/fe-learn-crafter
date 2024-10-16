c40ec6564afef91f3177380f26d8b4f31f3fa35f
Linh Hoang <linhhoang981221@gmail.com>
2024-10-16 12:20:11 +0700

    setup shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+import {Button} from "@/components/ui/button.tsx";
 
 function App() {
   return (
       <h1 className="text-3xl font-bold underline">
         Hello world!
+        <Button>Button</Button>
       </h1>
   )
 }
